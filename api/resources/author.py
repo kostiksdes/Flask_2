28744702ab027954c5ae67ba0db70cf373977cec
student <kostiksdes@gmail.com>
2022-09-18 10:40:40 +0300

    Initial Commit

--- api/resources/author.py
+++ api/resources/author.py
@@ -1,15 +1,18 @@
 from api import Resource, reqparse, db
 from api.models.author import AuthorModel
-from api.schemas.author import author_schema, authors_schema
+from api.schemas.author import AuthorSchema, authors_schema, author_schema
 
 
 class AuthorResource(Resource):
+    #          ma       flask
+    # Object ----> dict ----> JSON
     def get(self, author_id=None):  # Если запрос приходит по url: /authors
         if author_id is None:
             authors = AuthorModel.query.all()
-                        return authors_schema.dump(authors)
-            #authors_list = [author.to_dict() for author in authors]
-            #return authors_list, 200
+            # return AuthorSchema(many=True).dump(authors)
+            return authors_schema.dump(authors)
+            # authors_list = [author.to_dict() for author in authors]
+            # return authors_list, 200
 
         # Если запрос приходит по url: /authors/<int:author_id>
         author = AuthorModel.query.get(author_id)
@@ -25,7 +28,7 @@ class AuthorResource(Resource):
         author = AuthorModel(author_data["name"])
         db.session.add(author)
         db.session.commit()
-        return author.to_dict(), 201
+        return author_schema.dump(author), 201
 
     def put(self, author_id):
         parser = reqparse.RequestParser()
--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from api import api, app
-from api.resources.quote import QuoteResource
+from api.resources.quote import QuoteResource, QuoteListResource
 from api.resources.author import AuthorResource
 from config import Config
 
-
-api.add_resource(QuoteResource,
-                 '/authors/<int:author_id>/quotes/<int:quote_id>',
+api.add_resource(QuoteListResource,
                  '/authors/<int:author_id>/quotes',
                  '/quotes'
-                 )  # <-- requests
+                 )  # <-- GET
+api.add_resource(QuoteResource,
+                 '/authors/<int:author_id>/quotes/<int:quote_id>',
+                 '/authors/<int:author_id>/quotes'
+                 )  # <-- GET/POST/PUT/DELETE
 api.add_resource(AuthorResource,
                  '/authors/<int:author_id>',
                  '/authors')  # <-- requests
